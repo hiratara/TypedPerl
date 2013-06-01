478eb7c22bcbb41ff0486513fc9e87a6f6c19f86
Masahiro Honma <hiratara@cpan.org>
2013-06-01 11:31:19 +0900

    Implement eval and eval1

--- chapter4.hs
+++ chapter4.hs
@@ -1,5 +1,8 @@
+import Control.Monad
+
 data Info
 instance Show Info where show = const "Info"
+dummyInfo = undefined :: Info
 
 data Term = TmTrue Info
           | TmFalse Info
@@ -20,3 +23,19 @@ isVal (TmTrue _) = True
 isVal (TmFalse _) = True
 isVal t | isNumericVal t = True
         | otherwise = False
+
+eval1 :: Term -> Maybe Term
+eval1 (TmIf _ (TmTrue _) t _) = Just t
+eval1 (TmIf _ (TmFalse _) _ t) = Just t
+eval1 (TmIf info t1 t2 t3) = (\t1' -> TmIf info t1' t2 t3) `fmap` eval1 t1
+eval1 (TmSucc info t) = (\t' -> TmSucc info t') `fmap` eval1 t
+eval1 (TmPred _ (TmZero _)) = Just (TmZero dummyInfo)
+eval1 (TmPred _ (TmSucc _ v)) | isNumericVal v = Just v
+eval1 (TmPred info t) = (\t' -> TmPred info t') `fmap` eval1 t
+eval1 (TmIsZero _ (TmZero _)) = Just (TmTrue dummyInfo)
+eval1 (TmIsZero _ (TmSucc _ v)) | isNumericVal v = Just (TmFalse dummyInfo)
+eval1 (TmIsZero info t) = (\t' -> TmIsZero info t') `fmap` eval1 t
+eval1 _ = Nothing
+
+eval :: Term -> Term
+eval t = maybe t eval (eval1 t)
