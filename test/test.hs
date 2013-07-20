1ba87c09bcc1efe733c96ca2510dcb9f3a87174b
Masahiro Honma <hiratara@cpan.org>
2013-07-20 19:17:53 +0900

    Run 'cabal test' appropriately.

--- test/test.hs
+++ test/test.hs
@@ -1,10 +1,12 @@
 module Main (main) where
+import Test.Framework
+import Test.Framework.Providers.HUnit
 import Test.HUnit
 import qualified TypedPerl.Test.Parser as Parser
 import qualified TypedPerl.Test.Inferance as Inferance
 
 main :: IO ()
-main = runTestTT testList >> return ()
+main = (defaultMain . hUnitTestToTests) testList
   where
     testList = TestList [
         Parser.tests
