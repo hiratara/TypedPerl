f37100925dbf801aba952b67cda1c6d3e1076e75
Masahiro Honma <hiratara@cpan.org>
2013-07-20 18:30:30 +0900

    Add CLI

--- src/typedperl.hs
+++ src/typedperl.hs
@@ -1,3 +1,22 @@
 module Main (main) where
+import System.Environment (getArgs)
+import System.Exit
+import System.IO
+import TypedPerl.Inferance
+import TypedPerl.Parsec
+import TypedPerl.Types
+
+die :: String -> IO a
+die message = do
+  hPutStrLn stderr message
+  exitFailure
+
 main :: IO ()
-main = print "now implementing"
+main = do
+  path <- getArgs
+  if null path then die "Specify the file name" else return ()
+  source <- readFile . head $ path
+  ast <- either (die . show) return (parsePerl source)
+  ty <- either (die . show) return (infer ast)
+  putStrLn (showPerlType ty)
+  return ()
